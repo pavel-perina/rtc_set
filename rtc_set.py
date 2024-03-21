7bc7124d09b94629b0ca40068646345757fc2752
Pavel Perina <pavel.perina@gmail.com>
2024-03-21 20:13:11 +0100

    replace obsolete function

--- rtc_set.py
+++ rtc_set.py
@@ -1,13 +1,16 @@
-import  serial
-from datetime import datetime
+#!/usr/bin/env python
+import serial
+from datetime import datetime, timezone
 import time
 
 dow = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-ser = serial.Serial("COM6", 9600)
-time.sleep(5)                      # Sleep cause Arduino is likely reseting now
-utc = datetime.utcnow()
+ser = serial.Serial("/dev/ttyUSB0", 9600)   # COM6 on Windows
+print("Sleeping for five seconds ...")
+time.sleep(5)                               # Sleep cause Arduino is likely reseting now
+utc = datetime.now(timezone.utc)
 str = dow[utc.isoweekday()%7] + ' ' + utc.strftime ('%Y-%m-%d %H:%M:%S') + '\n'
-ser.write(str.encode('utf-8'))     # Convert string to utf-8 (should be plain ascii)
+ser.write(str.encode('utf-8'))              # Convert string to utf-8 (should be plain ascii)
 ser.flush()
 print(str)
 time.sleep(2)
+
